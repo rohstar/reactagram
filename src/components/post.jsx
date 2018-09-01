e0c935de9ecf21c0ac07f1a2503a6c83337a57a4
Rohan Maheshwari <rohstar@gmail.com>
2018-08-31 22:03:56 -0400

    Adds static likes and images

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
 
 class Post extends Component {
-  state = {};
+  state = {
+    likes: this.props.likes,
+    likeImage: "/images/like.png"
+  };
+
+  onLike = () => {
+    this.setState({
+      likes: this.state.likes + 1,
+      likeImage: "/images/liked.png"
+    });
+  };
+
   render() {
     return (
       <div className="card" style={{ margin: 10 }}>
         <img
+          onDoubleClick={this.onLike}
           className="card-img-top"
           src={this.props.img}
           alt="Card image cap"
         />
         <h6 className="card-subtitle mb-2 text-muted" style={{ margin: 5 }}>
-          {this.props.likes} likes
+          <a onClick={this.onLike}>
+            <img src={this.state.likeImage} style={{ width: 30 }} />
+          </a>
+          {this.state.likes} likes
         </h6>
         <h5 className="card-title" style={{ padding: 10 }}>
           {this.props.caption}
