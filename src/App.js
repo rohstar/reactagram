c5b533c9e3311675acd306668d95f881b561f328
Rohan Maheshwari <rohstar@gmail.com>
2018-09-01 19:46:07 -0400

    Extracts Nav

--- src/App.js
+++ src/App.js
@@ -2,39 +2,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Feed from "./components/feed";
+import Nav from "./components/nav";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav
-          className="navbar navbar-expand-md fixed-top"
-          style={{ borderBottom: "1px black solid", background: "#fff" }}
-        >
-          <img src={"/images/logo.png"} width="40" height="40" alt="" />
-          <span style={{ borderLeft: "1px black solid" }}>
-            <a style={{ margin: "0px 10px" }} className="navbar-brand">
-              Reactagram
-            </a>
-          </span>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarsExampleDefault"
-            aria-controls="navbarsExampleDefault"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div
-            className="collapse navbar-collapse"
-            id="navbarsExampleDefault"
-          />
-        </nav>
-
-        <main role="main" className="container" style={{ marginTop: 100 }}>
+        <Nav />
+        <main role="main" className="container" style={{ marginTop: 50 }}>
           <Feed />
         </main>
       </React.Fragment>
---
+++ src/components/nav.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  state = {};
+  render() {
+    return (
+      <nav
+        className="navbar navbar-expand-lg sticky-top"
+        style={{
+          borderBottom: "1px rgba(0,0,0,.0975) solid",
+          background: "#fff"
+        }}
+      >
+        <img src={"/images/logo.png"} width="40" height="40" alt="" />
+        <span style={{ borderLeft: "1px black solid" }}>
+          <a style={{ margin: "0px 10px" }} className="navbar-brand">
+            Reactagram
+          </a>
+        </span>
+        <form class="form-inline my-2 my-lg-0 p-2 right">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </form>
+        <div className="collapse navbar-collapse" id="navbarsExampleDefault" />
+      </nav>
+    );
+  }
+}
+
+export default Nav;
