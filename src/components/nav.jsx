942e1256cec963a81b16373bebec809458aa9424
Rohan Maheshwari <rohstar@gmail.com>
2018-09-02 09:57:45 -0400

    Navbar responsiveness

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,60 +5,69 @@ class Nav extends Component {
   render() {
     return (
       <nav
-        className="navbar sticky-top justify-content-center"
+        className="navbar navbar-expand-lg sticky-top justify-content-center"
         style={{
           borderBottom: "1px rgba(0,0,0,.0975) solid",
           background: "#fff"
         }}
       >
-        <div className="mr-auto m-2">
-          <img src={"/images/logo.png"} width="50" height="50" alt="" />
-          {/* <span style={{ borderLeft: "1px black solid" }}> */}
-          <a style={{ margin: "0px 0px" }} className="navbar-brand">
-            Reactagram
-          </a>
-        </div>
-        {/* </span> */}
-        <form class="form-inline mx-auto" style={{ textAlign: "right" }}>
-          <input
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-            style={{ width: "150%" }}
-          />
-        </form>
-        <div className="ml-auto m-2">
-          <a href="#">
-            <img
-              src={"/images/compass.png"}
-              width="30"
-              height="30"
-              alt=""
-              className="ml-3 mr-3"
-            />
-          </a>
-          <a href="#">
-            <img
-              src={"/images/hearts.png"}
-              width="40"
-              height="40"
-              alt=""
-              className="ml-3 mr-3"
-            />
-          </a>
-          <a href="#">
-            <img
-              src={"/images/person.png"}
-              width="30"
-              height="30"
-              alt=""
-              className="ml-3 mr-3"
-            />
-          </a>
-        </div>
+        <img src={"/images/logo.png"} width="50" height="50" alt="" />
+        <a style={{ margin: "0px 0px" }} className="navbar-brand">
+          Reactagram
+        </a>
 
-        <div className="collapse navbar-collapse" id="navbarsExampleDefault" />
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="form-inline mx-auto" style={{ textAlign: "right" }}>
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              style={{ width: "150%" }}
+            />
+          </form>
+          <div className="ml-auto m-2">
+            <a href="#">
+              <img
+                src={"/images/compass.png"}
+                width="30"
+                height="30"
+                alt=""
+                className="ml-3 mr-3"
+              />
+            </a>
+            <a href="#">
+              <img
+                src={"/images/hearts.png"}
+                width="40"
+                height="40"
+                alt=""
+                className="ml-3 mr-3"
+              />
+            </a>
+            <a href="#">
+              <img
+                src={"/images/person.png"}
+                width="30"
+                height="30"
+                alt=""
+                className="ml-3 mr-3"
+              />
+            </a>
+          </div>
+        </div>
       </nav>
     );
   }
